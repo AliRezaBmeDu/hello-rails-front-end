e0d635c824f574ec64c7339fa9d738c8766dd95c
AliReza <ali.reza.buet@gmail.com>
2023-12-07 14:46:29 +0600

    Add the design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Greeting from './components/Greeting'
+import Greeting from './components/Greeting';
 
-const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Greeting/>} />
-    </Routes>
-  );
-};
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Greeting />} />
+  </Routes>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import App from './App.js';
+import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
